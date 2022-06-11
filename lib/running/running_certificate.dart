98981a3d0c7fa549ac957eec7ce5e8a7eea16beb
CHOSION <chosion0107@gmail.com>
2022-06-11 16:13:56 +0900

    220611_6

---
+++ lib/running/running_certificate.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:runiverse/config/palette.dart';
+import 'package:runiverse/config/font.dart';
+
+class RunningCertificate extends StatefulWidget {
+  const RunningCertificate({Key? key}) : super(key: key);
+
+  @override
+  State<RunningCertificate> createState() => _RunningCertificateState();
+}
+
+class _RunningCertificateState extends State<RunningCertificate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
